1fc209f1e79461579437bf3bcc73cc6f0e4a7d3e
w <weverhall@gmail.com>
2023-08-31 20:21:19 +0300

    change port

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -1,12 +1,12 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:10000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
   it('can go from front page to pokemon page', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:10000')
     cy.contains('ivysaur').click()
     cy.contains('chlorophyll')
   })
