6078ffa81e93410011a39346fcee8a92e7758dac
w <weverhall@gmail.com>
2023-08-31 19:58:42 +0300

    change port

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('dist'))
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
